90322b430b64eccb5f645afc3af993c1d9cc2f8b
Reyzon <reyzon.thapa2020@gmail.com>
2023-04-20 08:24:33 +0545

    s

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 8880;
 const app = require('./app')
 const server = http.createServer(app);
-// const { sequelize } = require('./models')
+// const { sequelize } = require('./models');
 
 
 
