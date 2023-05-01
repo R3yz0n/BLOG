65a8720ccf661379cbadfc0029683918c71d0c97
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-01 19:24:35 +0545

    added fetching all post feature

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,14 +1,37 @@
+const express = require('express')
+const { posts } = require('../models')
+
+
+const getPosts = async (req, res) => {
+
+    try {
+
+        const query = req.query.category ? { where: { category: req.query.category } } : {};
+        console.log('----------');
+        console.log(query);
+
+        const allPosts = await posts.findAll(query);
+        res.json(allPosts);
+
+    } catch (err) {
+        if (err.name === 'SequelizeDatabaseError') {
+            res.status(400).json({ message: 'Invalid query parameter' });
+        }
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+
+}
+const getPost = () => {
+    const q = req.query.category ? "Select * from posts where cat=?" :
+        "Select * from posts"
+    db.query(q, [req.query.category], (err, data) => {
+        if (err)
+            return res.send(err)
+    })
+
+}
 const addPost = (req, res) => {
     res.json("form controller");
-
-
-}
-
-const getPost = () => {
-
-
-}
-const getPosts = () => {
 
 
 }
