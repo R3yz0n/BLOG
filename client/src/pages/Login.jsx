80061b783d1fbab2a71972f614ea4b2430ae99cb
rejina <thapachhettrirejina7@gmail.com>
2023-05-23 09:04:16 +0545

    updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { BsEyeFill } from 'react-icons/bs'
-import { ImUser } from 'react-icons/im'
+import { FaUserAlt } from 'react-icons/fa'
+import { AiFillLock } from 'react-icons/ai'
 import axios from 'axios'
 import { useFormik } from 'formik'
 import { loginSchmea } from '../schema'
@@ -58,14 +58,79 @@ const Login = () => {
 
 
 
-            <form className='flex flex-col sm:w-[30rem] md:w-[50rem]  border-2 m-auto sm:min-h-[25rem]  p-10 gap-6 shadow-2xl  rounded-[1em]' onSubmit={handleSubmit}>
+            <form className='flex  sm:min-h-[35rem] sm:min-w-[30rem] md:w-[50rem] w-[20rem] h-[35rem]  border-2 m-auto sm:p-10 md:gap-6 shadow-2xl  rounded-[1em]' onSubmit={handleSubmit}>
 
-                <section className='flex md:w-[95%] gap-[5rem] m-auto'>
-                    <div className='  w-[50%]'>
+                <section className='h-[32rem] min-w-fit flex md:flex-row flex-col md:w-[95%] md:gap-[5rem] m-auto'>
+                   
+                   
+                    <div className='ml-[10rem] sm:ml-[15rem] md:ml-[0rem] w-[6rem] md:w-[50%]'>
                         <img src="http://inmas-rangpur.org/Content/img/signin-image.jpg" alt="error404" />
 
 
-                        <p className='mx-auto flex text-end h-[4rem]'>
+                        
+                    </div>
+
+
+
+                    <div className=' md:w-[50%]'>
+                        <h1 className='text-[2.1rem] mx-auto font-[900] font-sans'>Log In</h1>
+
+
+                        <div className='flex flex-col space-y-[2rem] h-[8rem] '>
+
+                           <aside>
+
+
+                            <p className='  flex gap-[1.5rem] border-b-2 border-[#908c8ca0] mt-[2rem]  '>
+                
+                           
+                            <FaUserAlt className='h-[2rem]  ' />  <input type="text" placeholder='Email' className=' outline-none' name='userName'
+                                    value={values.userName} onChange={handleChange} onBlur={handleBlur} />
+                        
+                            </p>
+                           
+                        
+                            {errors.userName && touched.userName ? <p className='  text-red-600'>{errors.userName}</p> : null}
+                          
+                            </aside>
+                          
+                          
+                          
+                          
+                            <aside>
+                               
+                               <p className='flex gap-[1.3rem] border-b-2 border-[#908c8ca0]'>
+
+
+
+                           < AiFillLock className=' h-[2rem] w-[1.2rem]'/>
+                                <input type="text" placeholder='Password' className='  outline-none' name='password' value={values.password}
+                                    onChange={handleChange} onBlur={handleBlur} />
+                               
+                          
+            
+                                </p>
+                          
+                          
+                                {errors.password && touched.password ? <p className='text-red-600'>{errors.password}</p> : null}
+                          
+                            </aside>
+
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+                            <p className='my-[1.4rem]'> <input type="checkbox" /> Remember me </p>
+                            {response.length !== 0 && <p className='text-red-600 mx-auto my-[-2px]'>{response}</p>}
+
+                            <button className='mx-auto w-full py-2 mt-[0.5rem] rounded-[0.5rem] text-white h-[3.4rem] border-2 bg-[#70abe6]' type='submit'>Log in</button>
+
+
+
+                            <p className='-mt-3 flex text-end h-[4rem]'>
 
 
 
@@ -77,35 +142,6 @@ const Login = () => {
 
                         </p>
 
-                    </div>
-
-
-
-                    <div className='w-[50%]'>
-                        <h1 className='text-[2.1rem] mx-auto font-[900] font-sans'>Log In</h1>
-
-
-                        <div className='flex flex-col space-y-[2rem] h-[8rem] '>
-
-
-
-
-                            <aside>
-                                <input type="text" placeholder='Your Name' className=' border-b-2 border-[#908c8ca0]  w-[100%] h-[2rem] mt-[2rem] outline-none' name='userName'
-                                    value={values.userName} onChange={handleChange} onBlur={handleBlur} />
-                                {errors.userName && touched.userName ? <p className='text-red-600'>{errors.userName}</p> : null}
-                            </aside>
-
-                            <aside>
-                                <input type="text" placeholder='Password' className=' border-b-2 border-[#908c8ca0] w-[100%] h-[2rem outline-none' name='password' value={values.password}
-                                    onChange={handleChange} onBlur={handleBlur} />
-                                {errors.password && touched.password ? <p className='text-red-600'>{errors.password}</p> : null}
-                            </aside>
-
-                            <p className='my-[1.4rem]'> <input type="checkbox" /> Remember me </p>
-                            {response.length !== 0 && <p className='text-red-600 mx-auto my-[-2px]'>{response}</p>}
-
-                            <button className='mx-auto w-full py-2 mt-[0.5rem] rounded-[0.5rem] text-white h-[3.4rem] border-2 bg-[#70abe6]' type='submit'>Log in</button>
                         </div>
 
 
